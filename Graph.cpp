24af363472b44fe159ac9393cfb49748cdb923af
Andrew Fernandez <fdez26@ymail.com>
2019-04-10 01:28:36 -0400

    debugging removeEdge

--- Graph.cpp
+++ Graph.cpp
@@ -74,6 +74,7 @@ void Graph::removeEdge(std::string label1, std::string label2) {
                     ((e->get_endpoint2() == label1) || (e->get_endpoint2() == label2))) { // if the vertex appears at all 
                         std::cout << "inside this stupid fucking if statement" << std::endl;
                         (v->second).erase(e); //remove it
+                        std::cout << "just erased this edge" << std::endl;
                         // removed the edge
                         // break; // finished with this //NO WE CANT HAVE THIS BC OF THE CASE WHEN IT IS THE VERTEX
                 }
