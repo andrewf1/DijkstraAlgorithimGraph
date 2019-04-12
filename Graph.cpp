a5e05296bbc2d66ca6aee6a343cae96e8823edc9
Andrew Fernandez <fdez26@ymail.com>
2019-04-11 21:30:30 -0400

    add implementation of shortestPath using PQ

--- Graph.cpp
+++ Graph.cpp
@@ -259,11 +259,11 @@ unsigned long Graph::shortestPath(std::string startLabel, std::string endLabel, 
                         continue;
                     }
                     else {
+                        path.push_back(str1);
                         B = *adjacency_list.at(i);
                     }
                 }
                 else {
-                    path.push_back(str1);
                     B = x->get_endpoint();
                 }
                 auto weight = x->get_weight();
