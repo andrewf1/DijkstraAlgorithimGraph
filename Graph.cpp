1968a3bf4a1cf8cf31f376c139e0211951349448
Andrew Fernandez <fdez26@ymail.com>
2019-04-11 22:27:26 -0400

    add implementation of shortestPath using PQ

--- Graph.cpp
+++ Graph.cpp
@@ -1,32 +1,36 @@
 #include "Graph.hpp"
 #include "Dijkstra.hpp"
 
+//Graph Destructor
 Graph::~Graph() {
     for(unsigned int i = 0; i < adjacency_list.size(); i++) {
         for (auto edge : adjacency_list.at(i).get_edge_list()){
+            // delete every edge for every vertex in the graph
             delete edge;
         }
     }
+    // clear the adjacency_list for good measure
     adjacency_list.clear();
 }
 
 void Graph::addVertex(std::string label) {
-    Vertex v{label};
+    //create vertex object and push back to adjacency list
+    Vertex v{label}; 
     adjacency_list.push_back(v);
 }
 
 void Graph::removeVertex(std::string label) {
     unsigned int i = 0;
     while (i < adjacency_list.size()) {
-        if (*adjacency_list.at(i) == label) {
-            auto pos_to_delete = adjacency_list.begin() + i;
-            adjacency_list.erase(pos_to_delete);
+        if (*adjacency_list.at(i) == label) { // if found the label of the vertex to remove
+            auto pos_to_delete = adjacency_list.begin() + i; //get iterator to remove
+            adjacency_list.erase(pos_to_delete); //erase the element at the iterator
         }
-        else {
+        else { // the label is not equal to the vertex the loop is at
             auto elist = adjacency_list.at(i).get_edge_list();
-            for (auto e = elist.begin(); e != elist.end(); e++) {
+            for (auto e = elist.begin(); e != elist.end(); e++) { // for every edge in the vertex
                 auto edge = *e;
-                if(edge->get_endpoint() == label) {
+                if(edge->get_endpoint() == label) { // delete all occurrances of the label of the vertex to remove
                     adjacency_list.at(i).remove_edge(edge);
                 }
             }
@@ -36,32 +40,39 @@ void Graph::removeVertex(std::string label) {
 }
 
 void Graph::addEdge(std::string label1, std::string label2, unsigned long weight) {
+    // create 2 new Edge pointer objects for each combo of the labels given having the same weight
     Edge* vertex_edge = new Edge(label2, weight);
     Edge* endpoint_edge = new Edge(label1, weight);
+    //for every vertex in the graph
     for (unsigned int i = 0; i < adjacency_list.size(); i++) {
-        if (*adjacency_list.at(i) == label1) {
+        if (*adjacency_list.at(i) == label1) { // if the vertex is the first label
+            //push back the appropriate edge
             adjacency_list.at(i).push_back_edge(vertex_edge); 
         }
-        else if (*adjacency_list.at(i) == label2) {
+        else if (*adjacency_list.at(i) == label2) { // if the vertex is the second label
+            // push back the appropriate edge
             adjacency_list.at(i).push_back_edge(endpoint_edge);
         }
     }
 }
 
 void Graph::removeEdge(std::string label1, std::string label2) {
+    // for every vertex in the graph
     for (unsigned int i = 0; i < adjacency_list.size(); i++) {
         auto elist = adjacency_list.at(i).get_edge_list();
         auto e = elist.begin();
-        while (e != elist.end()) {
+        while (e != elist.end()) { // for every edge of the vertex
             auto edge = *e;
-            if(*adjacency_list.at(i) == label1) {
+            if(*adjacency_list.at(i) == label1) { 
                 if (edge->get_endpoint() == label2) {
+                    // found the appropiate edge and can remove
                     adjacency_list.at(i).remove_edge(edge);
                     break;
                 }
             }
             else if (*adjacency_list.at(i) == label2) {
                 if (edge->get_endpoint() == label1) {
+                    //found edge and remove
                     adjacency_list.at(i).remove_edge(edge);
                     break;
                 }
@@ -71,71 +82,83 @@ void Graph::removeEdge(std::string label1, std::string label2) {
     }
 }
 
+// utility function to get the Vertex object given a string
 Vertex Graph::at(std::string label)  {
     for (unsigned int i = 0; i < adjacency_list.size(); i++) {
         if (*adjacency_list.at(i) == label) {
             return adjacency_list.at(i);
         }
     }
-    Vertex v;
+    Vertex v; // return an empty object if could not find 
     return v;
 }
 
-//Dijkstra's Algorithm
-unsigned long Graph::shortestPath(std::string startLabel, std::string endLabel, std::vector<std::string> &path) {
-    std::priority_queue<std::pair<int, std::string>, std::vector<std::pair<int, std::string>>, std::greater<std::pair<int, std::string>>> pq;
-    std::map<std::string, int> distance_from_start;
-    std::map<std::string, std::string> previous;
-
-    for (unsigned int i = 0; i < adjacency_list.size(); i++) {
-        distance_from_start[*adjacency_list.at(i)] = INT_MAX;
-        previous[*adjacency_list.at(i)] = "";
-    }
-
-    pq.push(std::make_pair(0, startLabel));
-    distance_from_start[startLabel] = 0;
-    do {
-        std::string curr_vertex = pq.top().second;
-        pq.pop();
-        std::string neighbor;
-
-        for (unsigned int i = 0; i < adjacency_list.size(); i++) {
-            for (auto edge : adjacency_list.at(i).get_edge_list()) {
-                if(!(*adjacency_list.at(i) == curr_vertex)) {
-                    if(!(edge->get_endpoint() == curr_vertex)) {
-                        continue;
-                    }
-                    else {
-                        neighbor = *adjacency_list.at(i);
-                    }
-                }
-                else {
-                    neighbor = edge->get_endpoint();
-                }
-                auto weight = edge->get_weight();
-                if(!(distance_from_start[neighbor] > distance_from_start[curr_vertex] + weight)) {
-                    continue;
-                }
-                previous[neighbor] = curr_vertex;
-                distance_from_start[neighbor] = distance_from_start[curr_vertex] + weight;
-                pq.push(std::make_pair(distance_from_start[neighbor], neighbor));
-            }
-        }
-    } while (!pq.empty());
-
-    path.push_back(startLabel);
-    createPath(path, previous, endLabel);
-    return distance_from_start[endLabel];
-
-}
-
-void Graph::createPath(std::vector<std::string>& path, std::map<std::string, std::string> previous, std::string endLabel) {
+//  utility function called recursivley to complete the path
+void createPath(std::vector<std::string>& path, std::map<std::string, std::string> previous, std::string endLabel) {
     if (previous[endLabel] != "") { //if it is not the empty string
-        createPath(path, previous, previous[endLabel]);
+        createPath(path, previous, previous[endLabel]); // recursive call
         path.push_back(endLabel);
     }
     else { //return void when the endLabel = ""
         return;
     }
+}
+
+//Dijkstra's Algorithm
+unsigned long Graph::shortestPath(std::string startLabel, std::string endLabel, std::vector<std::string> &path) {
+    //data structures to implement the algorithm
+    std::priority_queue<std::pair<int, std::string>, std::vector<std::pair<int, std::string>>, std::greater<std::pair<int, std::string>>> pq;
+    std::map<std::string, int> distance_from_start;
+    std::map<std::string, std::string> previous;
+
+    //for each vertex in the graph
+    for (unsigned int i = 0; i < adjacency_list.size(); i++) {
+        // add them to each map with default values
+        distance_from_start[*adjacency_list.at(i)] = INT_MAX;
+        previous[*adjacency_list.at(i)] = "";
+    }
+    // push the first vertex into the pq
+    pq.push(std::make_pair(0, startLabel));
+    distance_from_start[startLabel] = 0; //initial distance is just to itself
+    do {
+        std::string curr_vertex = pq.top().second; // get a current_vertex
+        pq.pop(); //pop it from the queue
+        std::string neighbor;
+
+        // for each vertex in the graph
+        for (unsigned int i = 0; i < adjacency_list.size(); i++) {
+            for (auto edge : adjacency_list.at(i).get_edge_list()) { //for each edge at the vertex
+                // check if the loop is at the current vertex
+                if(!(*adjacency_list.at(i) == curr_vertex)) {
+                    if(!(edge->get_endpoint() == curr_vertex)) {
+                        continue;
+                    }
+                    else {
+                        // if the endpoint at the edge is the current vertex
+                        neighbor = *adjacency_list.at(i);
+                    }
+                }
+                else { // if not the current vertex
+                    /// set the edge to be the neighbor
+                    neighbor = edge->get_endpoint();
+                }
+                auto weight = edge->get_weight();
+                unsigned long distance_to_neighbor = distance_from_start[neighbor]; 
+                unsigned long distance_to_curr_vertex = distance_from_start[curr_vertex];
+                if(!(distance_to_neighbor > distance_to_curr_vertex + weight)) {
+                    // do nothing if the distance is not greater than the curr_vertex plus the curr edge's weight
+                    continue;
+                }
+                previous[neighbor] = curr_vertex; // set the previous vertex of the neighbor to the curr_vertex
+                distance_from_start[neighbor] = distance_from_start[curr_vertex] + weight; //set the new distance
+                pq.push(std::make_pair(distance_from_start[neighbor], neighbor)); // push back into queue
+            }
+        }
+    } while (!pq.empty());
+
+    path.push_back(startLabel);
+    createPath(path, previous, endLabel); // finish creating path
+    return distance_from_start[endLabel]; // return the weight of the entire path (start->endLabel)
+
 }
 
--- Graph.hpp
+++ Graph.hpp
@@ -6,48 +6,62 @@
 #include <limits.h>
 #include <queue>
 #include <map>
-//#include <tuple>
 
 class Edge { 
 public:
+    //constructor
     Edge(std::string label1, unsigned long _weight) :
         endpoint{label1}, weight{_weight} {}
+    //destructor
+    ~Edge() = default;
+    //accessors
     std::string get_endpoint() const { return endpoint; }
     unsigned long get_weight() const { return weight; }
-private:
-    std::string endpoint;
-    unsigned long weight;
+private: // data members
+    std::string endpoint; //other endpoint (not vertex)
+    unsigned long weight; //weight of the edge
 };
 
 class Vertex {
 public:
-    Vertex(std::string held = "") : data{held} {};
-    std::string operator*() { return data; }; //return element at vertex
+    //constructor
+    Vertex(std::string held = "") : element{held} {};
+    //destructor
+    ~Vertex() = default;
+
+    std::string operator*() { return element; }; //return element at vertex
+    
+    // utility functions to modify Vertex object's data members
     void push_back_edge(Edge* e) { edge_list.push_back(e); }
     void remove_edge(Edge* edge) { edge_list.remove(edge); }
+
+    // edge_list accessor
     std::list<Edge*> get_edge_list() { return edge_list; }
-    bool isAdjacentTo(Vertex v);
 private:
-    std::string data;
-    std::list<Edge*> edge_list;
+    std::string element;
+    std::list<Edge*> edge_list; //every edge connected to the Vertex
 };
 
 class Graph : public GraphBase {
 public:
+    //constructor
     Graph() = default;
+    //destructor
     ~Graph(); 
+    //utility functions
+    Vertex at(std::string label); //returns the element of a vertex at the string
 
-    Vertex at(std::string label);
-    void createPath( std::vector<std::string>& path, std::map<std::string, std::string> parent, std::string endLabel);
+    //accessor
     std::vector<Vertex> get_adjacency_list() const { return adjacency_list; } 
 
+    // METHODS TO IMPLEMENT FOR ASSIGNMENT
     virtual void addVertex(std::string label); //inserts a new vertex storing the element
     virtual void removeVertex(std::string label);  // remove vertex containing the element and all its incident edges
     virtual void addEdge(std::string label1, std::string label2, unsigned long weight); //insert a new undirected edge with end vertices
     virtual void removeEdge(std::string label1, std::string label2); // remove the edge between the two vertices given
-    virtual unsigned long shortestPath(std::string startLabel, std::string endLabel, std::vector<std::string> &path);    
+    virtual unsigned long shortestPath(std::string startLabel, std::string endLabel, std::vector<std::string> &path); //calculates the weight of the shortestPath of all verticies
 private:
-    // std::vector<std::pair<Vertex, std::list<Edge>>> adjacency_list;
+    //adjacency_list that holds Vertex objects
     std::vector<Vertex> adjacency_list;
 };
 
